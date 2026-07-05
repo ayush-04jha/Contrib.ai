eb14c722f7b114ac37051b1abf45378d8f3edcc2
Ayush Jha <ayu.jha321@gmail.com>
2026-07-05 22:57:42 +0530

    Fix cookie sameSite configuration for cross-domain auth

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -61,9 +61,8 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: "None",
+            sameSite: 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            
             path: '/'
         });
         
