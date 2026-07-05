aa697d0def683b09a18427c6cd06e8d694a366ac
Ayush Jha <ayu.jha321@gmail.com>
2026-07-05 20:47:44 +0530

    change middilware

--- server/middilware/middilware.js
+++ server/middilware/middilware.js
@@ -3,6 +3,9 @@ import jwt from "jsonwebtoken";
 export const authMiddleware = (req, res, next) => {
   try {
     const token = req.cookies ? req.cookies.token : null;
+    console.log("Auth Middleware - Cookies:", req.cookies);
+    console.log("Auth Middleware - Token:", token);
+    
     if (!token) {
       return res.status(401).json({
         message: "Access Denied. No token provided."
@@ -16,6 +19,7 @@ export const authMiddleware = (req, res, next) => {
     next();
   }
   catch (e) {
+    console.log("Auth Middleware Error:", e);
     return res.status(401).json({
       message: "Invalid or expired token"
     });
