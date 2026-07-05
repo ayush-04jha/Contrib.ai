1b70b45e0e453c9f8e1dbf370f23605fc7f2e736
Ayush Jha <ayu.jha321@gmail.com>
2026-07-05 22:15:54 +0530

    fix login issue

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -61,7 +61,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
+            sameSite: process.env.NODE_ENV === 'production' ? "None" : "Lax",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             domain: '.onrender.com', // Allow all subdomains
             path: '/'
