42d620f4e569ae95b8d9a1f33fead4f7ccaf47b1
Ayush Jha <ayu.jha321@gmail.com>
2026-07-07 01:52:49 +0530

    fix socket

--- client/src/socket.tsx
+++ client/src/socket.tsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8080";
+const URL = import.meta.env.VITE_API_BASE_URL?.replace('/api', '') || "http://localhost:8080";
 
 export const socket = io(URL, {
   autoConnect: false, 
